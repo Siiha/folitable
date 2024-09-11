d5768b1a2e3a40823f5e3298f1d458e604ff90d3
Siiha <90766306+Siiha@users.noreply.github.com>
2024-09-11 07:10:19 +0300

    Add files via upload

--- foli.py
+++ foli.py
@@ -1,41 +1,41 @@
 import requests
 from datetime import datetime
 from tkinter import Tk,ttk
-s = requests.Session()
-w = Tk()
+sessio = requests.Session()
+start_window = Tk()
 w2 = Tk()
 def foli(x):
 	dt = datetime.now()
-	a = s.get(f'https://data.foli.fi/siri/sm/{x}')
+	a = sessio.get(f'https://data.foli.fi/siri/sm/{x}')
 	a.raise_for_status()
 	a = a.json()
 	r = []
-	for i in range(3):
-		b = datetime.fromtimestamp(a['result'][i]['expectedarrivaltime'])
+	for i in a['result'][:3]:
+		b = datetime.fromtimestamp(i['expectedarrivaltime'])
 		c = b-dt
 		d = round(c.total_seconds()/60)
-		l = a['result'][i]['lineref']
+		l = i['lineref']
 		r.append((l,d))
 	return r
 table = ttk.Label(master=w2, font="Cantarell 25")
 def output(x):
-	w.destroy()
+	start_window.destroy()
 	update(x)
 def update(x):
 	f = foli(x)
 	table_text=""
-	for i in range(3):
-		table_text+=f"{f[i][0]} : {f[i][1]}\n"
+	for (i,j) in f:
+		table_text+=f"{i} : {j}\n"
 	table.config(text=table_text)
 	table.pack()
 	w2.after(60000,update,x)
 def start(w):
-	l1 = ttk.Label(master=w,text="Give bus stop id.")
-	entry = ttk.Entry(master=w)
-	b = ttk.Button(master=w,text="ok",command=lambda: output(entry.get()))
+	l1 = ttk.Label(master=start_window,text="Give bus stop id.")
+	entry = ttk.Entry(master=start_window)
+	b = ttk.Button(master=start_window,text="ok",command=lambda: output(entry.get()))
 	l1.pack()
 	entry.pack()
 	b.pack()
-start(w)
-w.mainloop()
+start(start_window)
+start_window.mainloop()
 w2.mainloop()
